e245f77f66a83650318dd7316167a015722e88f2
haivl4nhan3bang16 <duy.haivl321@gmail.com>
2019-01-08 09:56:02 +0700

    Bài Thi Cuối Năm

---
+++ WindowsFormsApplication16/WindowsFormsApplication16/Hinh.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication16
+{
+
+    class Hinh
+    {
+        public List<Diem> diem = new List<Diem>();
+        public Hinh()
+        {
+
+        }
+        public void Load(Dictionary<string, List<Diem>> save, string saveName)
+        {
+            foreach(var item in save)
+            {
+                if(item.Key == saveName)
+                {
+                    diem = item.Value;
+                }
+            }
+        }
+        public List<Diem> SaveData()
+        {
+            List<Diem> temp = new List<Diem>();
+            temp = diem;
+            return temp;
+        }
+
+    }
+}
